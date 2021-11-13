6a65656a3aeef0e5fa24776750af0d16c1de3bfb
LHL670 <sally12066@gmail.com>
2021-11-13 21:06:51 +0800

    update

--- CGUScholarCrawl.py
+++ CGUScholarCrawl.py
@@ -77,6 +77,8 @@ def get_personalpage(id):
     headers = {
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36'
     }
-    r = requests.get(url, headers=headers)
-    soup = BeautifulSoup(r.text, "html.parser")
+    r = requests.get(url, headers=headers, proxies={
+                          "http": "http://0.0.0.0:8888"}).text
+    
+    soup = BeautifulSoup(r, "html.parser")
     return result(soup, id)
--- CGUScholar_personalPage.py
+++ CGUScholar_personalPage.py
@@ -6,7 +6,7 @@ import CGUScholarCrawl
 import checkDataformat
 import getIDQueue
 import CGUScholarLabel
-
+import requests
 # Worker 類別，負責處理資料
 
 
@@ -40,6 +40,7 @@ def LabelCrawl(type, updatelabel):  # if empty ,updatelabel is null
         label = manageFirebase.get_emptylabelname()  # limit
     elif(type == "reorganize"):
         label = updatelabel
+
     labellist = CGUScholarLabel.get_labelIDlist(label)
     check_labelformat = checkDataformat.labelinfoformat(labellist)
 
@@ -68,11 +69,39 @@ def CGUCrawlWorker(label):
     print("Done.")
 
 
+def testdocker():
+    while 1:
+        url = 'http://httpbin.org/ip'
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36',
+
+        }
+
+        ip = requests.get(url, headers=headers, proxies={
+                          "http": "http://0.0.0.0:8888"}).text
+
+        if ip:
+            print(ip)
+            time.sleep(10)
+    return
+
+
 # 累積到一定得筆數upload firebase
 if __name__ == '__main__':
     print('start')
-    label = manageFirebase.get_labelforCGUScholar()
+
     # update older label then crwal user profile
+
+    label = manageFirebase.get_labelforCGUScholar()
     LabelCrawl("reorganize", label)
     CGUCrawlWorker(label)
-    LabelCrawl("empty", None)  # add label ,userID is null
+
+    # label = manageFirebase.get_emptylabelname()  # limit
+    # LabelCrawl("empty", label)
+    # CGUCrawlWorker(label)
+
+    # # add label ,userID is null
+    # LabelCrawl("empty", None)
+
+    # for testing
+    # testdocker()
